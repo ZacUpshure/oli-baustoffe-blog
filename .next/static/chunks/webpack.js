bb052fda6290780251836622ca473b90a676d557
Zac <zac.upshure@outlook.com>
2023-07-11 11:35:17 +0200

    fixed similar posts issue

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -161,7 +161,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0814bfa4f519743b"; }
+/******/ 		__webpack_require__.h = function() { return "975b94b3fb044a74"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from 'react';
 import moment from 'moment';
 import Link from 'next/link';
 
-import { getRecentPosts, getSimilarPosts } from '../services';
+import { getRecentPosts, getRelatedPosts } from '../services';
 
 const PostWidget = ({ categories, slug }) => {
     const [relatedPosts, setRelatedPosts] = useState([]);
 
     useEffect(() => {
         if(slug) {
-            getSimilarPosts(categories, slug)
+            getRelatedPosts(categories, slug)
                 .then((result) => setRelatedPosts(result))
         } else {
             getRecentPosts()
