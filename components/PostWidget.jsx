91be8bf3427d1cfeeb08d4a9b477f30af3634a96
Zac <zac.upshure@outlook.com>
2023-07-11 11:30:37 +0200

    blog frontpage

---
+++ components/PostWidget.jsx
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from 'react';
+import moment from 'moment';
+import Link from 'next/link';
+
+import { getRecentPosts, getSimilarPosts } from '../services';
+
+const PostWidget = ({ categories, slug }) => {
+    const [relatedPosts, setRelatedPosts] = useState([]);
+
+    useEffect(() => {
+        if(slug) {
+            getSimilarPosts(categories, slug)
+                .then((result) => setRelatedPosts(result))
+        } else {
+            getRecentPosts()
+                .then((result) => setRelatedPosts(result))
+        }
+    }, [slug])
+
+    console.log(relatedPosts)
+    
+    return (
+        <div className='bg-white shadow-lg rounded-lg p-8 mb-8'>
+            <h3 className='text-xl mb-8 font-semibold border-b pb-4'>
+                {slug ? 'Verwandte Beiträge' : 'Neue Beiträge'}
+            </h3>
+            {relatedPosts.map((post) => (
+                <div key={post.title} className='flex items-center w-full mb-4'>
+                    <div className='w-16 flex-none'>
+                        <img 
+                         alt={post.title}
+                         width="60px"
+                         heigth="60px"
+                         src={post.featuredImage.url} 
+                         className='align-middle rounded-full' 
+                         />
+                    </div>
+                    <div className='flex-grow ml-4'>
+                        <p className='text-gray-500 font-xs'>
+                            {moment(post.createdAt).format('MMM DD, YYYY')}
+                        </p>
+                        <Link href={`/post/${post.slug}`} className='text-md' key={post.title}>
+                            {post.title}
+                        </Link>
+                    </div>
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export default PostWidget
